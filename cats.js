1bdb68e27537221f5db5196a5ea66f7170a0cd10
Vassily Krasnov <mykrasnov@gmail.com>
2013-03-25 12:32:54 +0200

    True way to GET json from API

--- cats.js
+++ cats.js
@@ -1,13 +1,24 @@
 (function($, undefined) {
 	$(function() {
 		// Альбом, с которым будем работать
-		var album = "http://api-fotki.yandex.ru/api/users/aig1001/";
+		var album = "http://api-fotki.yandex.ru/api/users/aig1001/album/63684/";
+
+		// Парсинг ответа сервера
+		function parsePage (data) {
+			var entries = data.entries;
+			
+			for (var i = 0; i < entries.length; i++) {
+				console.log( entries[i].imageCount );
+			};
+		}
 
 		// Получаем коллекцию фотографий из альбома
 		function getPage (argument) {
+			argument += "?format=json";
 			return $.ajax({
 				url: argument,
 				dataType: "jsonp",
+				jsonpCallback: "parsePage"
 			});
 			// console.log(data);
 		}
