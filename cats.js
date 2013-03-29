aa14aeaf395d4f67d63f5daee8f26e350b56ca32
Vassily Krasnov <mykrasnov@gmail.com>
2013-03-29 18:16:50 +0200

    Some changes

--- cats.js
+++ cats.js
@@ -1,38 +1,46 @@
 (function($, undefined) {
-	$(function() {
-		// Альбом, с которым будем работать
-		var album = "http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/?format=json";
+	// Альбом, с которым будем работать
+	var album = "http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/?format=json";
 
-		var nextPage; // ссылка на следующую страницу коллекции
-		var pics = [];
+	var nextPage; // ссылка на следующую страницу коллекции
+	var pics = [];
 
-		// Получаем коллекции фотографий из альбома
-		function getPage (argument) {
-			var currentImg = {};
-			$.ajax({
-				url: argument,
-				dataType: "jsonp",
-				success: function (data) {
-					for (var i = 0; i < data.entries.length; i++) {
-						$('.gallery').append('<img src="' + data.entries[i].img.XXS.href + '"\/>');
-						// pics.push({'thumb': data.entries[i].img.XXS.href, 'img': data.entries[i].img.orig.href});
-						currentImg.thumb = data.entries[i].img.XXS.href;
-						currentImg.big = data.entries[i].img.orig;
+	function showBigPicture (picture) {
+		$('.current-picture').append('<img src="' + picture.big.href + '" />');
+	}
+
+	// Получаем коллекции фотографий из альбома
+	function getPage (argument) {
+		var currentImg = {};
+		$.ajax({
+			url: argument,
+			dataType: "jsonp",
+			success: function (data) {
+				for (var i = 0; i < data.entries.length; i++) {
+					currentImg.thumb = data.entries[i].img.XXS.href;
+					currentImg.big = data.entries[i].img.orig;
+					pics.push(currentImg);
 						
-						pics.push(currentImg);
-					};
-					if (data.links.next) {
-						console.log("Go to page: " + data.links.next);
-						getPage(data.links.next);
-					} else {
-						console.log(pics);
-					}
+					$('.gallery')
+						.append('<img src="' + data.entries[i].img.XXS.href + '"\/>')
+							.on("click", function (event) {
+								showBigPicture(pics[i]);
+							});
+				};
+					
+				if (data.links.next) {
+					console.log("Go to page: " + data.links.next);
+					getPage(data.links.next);
+				} else {
+					console.log(pics);
+					showBigPicture(pics[0]);
 				}
-			});
-		}
+			}
+		});
+	}
 
+	$(function() {
 		getPage(album);
 		console.log(pics);
-				
 	});
 })(jQuery);
