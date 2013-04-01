d76c08a807a65f09a8071568fefe67d8dbf7f6c8
Vassily Krasnov <mykrasnov@gmail.com>
2013-04-01 11:28:34 +0300

    Обработка клика на маленькую картинку

--- cats.js
+++ cats.js
@@ -3,29 +3,33 @@
 	var album = "http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/?format=json";
 
 	var nextPage; // ссылка на следующую страницу коллекции
-	var pics = [];
+	var pics = [],
+		picsGlobalCount = 0;
+
 
 	function showBigPicture (picture) {
-		$('.current-picture').append('<img src="' + picture.big.href + '" />');
+		// $('.current-picture').append('<img src="' + pics[picture].big.href + '" />');
+		$('.current-picture').empty();
+		$('.current-picture').append('<img/>').find('img').attr('src', picture);
 	}
 
 	// Получаем коллекции фотографий из альбома
-	function getPage (argument) {
-		var currentImg = {};
+	function getPage (collection_url) {
 		$.ajax({
-			url: argument,
+			url: collection_url,
 			dataType: "jsonp",
 			success: function (data) {
 				for (var i = 0; i < data.entries.length; i++) {
+					var currentImg = {};
 					currentImg.thumb = data.entries[i].img.XXS.href;
 					currentImg.big = data.entries[i].img.orig;
 					pics.push(currentImg);
 						
 					$('.gallery')
-						.append('<img src="' + data.entries[i].img.XXS.href + '"\/>')
-							.on("click", function (event) {
-								showBigPicture(pics[i]);
-							});
+						// .append('<a href="' + currentImg.big.href + '"\/>')
+						.append('<a href="' + currentImg.big.href + '"><img id="' + picsGlobalCount + '" src="' + data.entries[i].img.XXS.href + '"\/></a>');
+						// .wrapInner('<a href="' + currentImg.big.href + '" />');
+					picsGlobalCount++;
 				};
 					
 				if (data.links.next) {
@@ -33,13 +37,21 @@
 					getPage(data.links.next);
 				} else {
 					console.log(pics);
-					showBigPicture(pics[0]);
+					showBigPicture(0);
 				}
 			}
 		});
 	}
 
+	function prepare () {
+		$(document).delegate('a', 'click', function (event) {
+			showBigPicture($(this).attr('href'));
+			return false;
+		});
+	}
+
 	$(function() {
+		prepare();
 		getPage(album);
 		console.log(pics);
 	});
