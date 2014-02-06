698952616102ca0a3351b403a31373505d5858c3
Vassily Krasnov <mykrasnov@gmail.com>
2014-02-06 13:56:27 +0200

    Обработка модификатора active для thumb.

--- desktop.blocks/thumb/thumb.js
+++ desktop.blocks/thumb/thumb.js
@@ -9,7 +9,26 @@ modules.define('i-bem__dom', ['BEMHTML'], function(provide, BEMHTML, DOM) {
 				
 				this.liveBindTo('click', function(e) {
 					e.preventDefault();
-					this.emit('click');
+					
+					// Если картинка не имеет модификатора active, то находим
+					// блок с таким модификатором, снимаем его и устанавливаем
+					// active для текущего блока.
+					if (!this.hasMod('active')) {
+						
+						// Убираем предыдущий active
+						// TODO: следующий блок кода необходимо заменить более элегантным решением.
+						// Возможно, пересмотреть взаимодействие блоков sliderInner и thumb.
+						var parent = this.findBlockOutside('sliderInner'),
+							thumbsArray = parent.findBlocksInside('thumb');
+						
+						for (var i = 0; i < thumbsArray.length; i++) {
+							if (thumbsArray[i].hasMod('active')) thumbsArray[i].delMod('active');
+						};
+
+						// Устанавливаем active на текущий блок
+						this.setMod('active');
+						this.emit('click');
+					}
 				});
 
 				return false;
